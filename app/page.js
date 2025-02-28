53467142746f3b644717ee75af999151cc0bcd50
Mitzi Vera <144722779+mitzi-vera@users.noreply.github.com>
2025-02-27 17:29:16 -0700

    updated page.js for flex

--- app/page.js
+++ app/page.js
@@ -13,9 +13,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <p>The student data DOB is Month Day Year - MM/DD/YYYY</p>
-        <StudentForm />
-        <StudentList onSelect={setSelectedStudent} />
-        <StudentDetails student={selectedStudent} />
+        <div className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
+          <StudentForm />
+          <StudentList onSelect={setSelectedStudent} />
+          <StudentDetails student={selectedStudent} />
+        </div>
       </main>
     </div>
   );
